fb42ff011e90c8dac89306c905e6971506b243ab
Esraa Mohamed Abodief <em4728644@gmail.com>
2023-08-01 13:32:12 +0300

    chore: add quick sort

---
+++ quick-sort.js
@@ -0,0 +1,22 @@
+//idea, an element is in the sorted position if all the elements on the left hand side is smaller than that element
+//and all the elements on the right hand side is greater than that element
+
+const quickSort = (arr) => {
+  if (arr.length <= 1) {
+    return arr;
+  }
+
+  let pivot = arr[0];
+  let leftArr = [];
+  let rightArr = [];
+
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] < pivot) {
+      leftArr.push(arr[i]);
+    } else {
+      rightArr.push(arr[i]);
+    }
+  }
+
+  return [...quickSort(leftArr), pivot, ...quickSort(rightArr)];
+};
